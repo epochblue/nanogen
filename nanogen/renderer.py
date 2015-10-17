a85e0a906ad457fefa1375c1a97ca11e1e703bc2
Bill Israel <bill.israel@gmail.com>
2015-10-17 18:31:29 -0500

    Fix minor issues in renderer.py

--- nanogen/renderer.py
+++ nanogen/renderer.py
@@ -1,11 +1,11 @@
 import mistune
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
-from pygments.formatters import HtmlFormatter
+from pygments.formatters.html import HtmlFormatter
 
 class NanogenRenderer(mistune.Renderer):
-    def block_code(self, code, lang):
-        if not lang:
+    def block_code(self, code, lang=None):
+        if lang is None:
             return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
         lexer = get_lexer_by_name(lang, stripall=True)
         formatter = HtmlFormatter()
