680cacf9a1eea4968894ec4dfe9a985820b11f31
Bill Israel <bill.israel@gmail.com>
2015-10-23 16:12:23 -0500

    Simplify file structure
    Move Post "model" into `nanogen.py`, which allowed me to move stuff in
    `__init__.py` out of there as well.

--- nanogen/__init__.py
+++ nanogen/__init__.py
@@ -1,16 +1,1 @@
-import os
-import re
-
 __version__ = '0.8.0'
-
-PATHS = {
-    'cwd': os.getcwd(),
-    'site': os.path.join(os.getcwd(), '_site'),
-    'posts': os.path.join(os.getcwd(), '_posts'),
-    'layouts': os.path.join(os.getcwd(), '_layouts'),
-}
-
-
-def slugify(text):
-    """Create a suitable slug for the given text"""
-    return re.sub(r'\W', '-', text).lower()
--- nanogen/nanogen.py
+++ nanogen/nanogen.py
@@ -10,12 +10,88 @@ import yaml
 import click
 import jinja2
 
-from .logger import log, init_logger
-from .models import Post
-from . import PATHS, slugify
+import logger
+import renderer
+
+
+FM_SEPARATOR = '----'
+PATHS = {
+    'cwd': os.getcwd(),
+    'site': os.path.join(os.getcwd(), '_site'),
+    'posts': os.path.join(os.getcwd(), '_posts'),
+    'layouts': os.path.join(os.getcwd(), '_layouts'),
+}
 
 JINJA_LOADER = jinja2.FileSystemLoader([PATHS['cwd'], PATHS['layouts']])
 JINJA_ENV = jinja2.Environment(loader=JINJA_LOADER)
+
+
+class Post(object):
+    """Represents a post."""
+
+    def __init__(self, path_to_file):
+        logger.log.info('Processing post at %s', path_to_file)
+        self.markdown = renderer.markdown
+        self.path = path_to_file
+        self.filename = self.path.split('/')[-1]
+
+        with open(self.path, 'r') as p:
+            p_full = p.read()
+
+        p_split = p_full.split(FM_SEPARATOR)
+        if len(p_split) == 3:
+            self.config = yaml.safe_load(p_split[1])
+            content_raw = p_split[2].strip()
+        else:
+            self.config = yaml.safe_load(p_split[0])
+            content_raw = p_split[1].strip()
+
+        self.content = self.markdown(content_raw)
+
+    def __getattr__(self, item):
+        """
+        Attempt to find the "missing" attribute in the post's configuration.
+        """
+        if item in self.config:
+            return self.config[item]
+        logger.log.warning('Unable to locate attribute %s', item)
+        return None
+
+    def __repr__(self):
+        return u'Post(path={})'.format(self.path)
+
+    @property
+    def pub_date(self):
+        year, month, day = map(int, self.filename.split('-', 3)[:3])
+        return datetime.datetime(year=year, month=month, day=day)
+
+    @property
+    def slug(self):
+        if 'slug' in self.config:
+            _slug = self.config['slug']
+        else:
+            _slug = '-'.join(self.filename.split('-', 3)[3:]).rsplit('.', 1)[0]
+        return _slug
+
+    @property
+    def html_filename(self):
+        return '{}.html'.format(self.slug)
+
+    @property
+    def permapath(self):
+        dt = self.pub_date
+        return os.path.join(PATHS['site'], str(dt.year),
+                            '{:02d}'.format(dt.month), self.html_filename)
+
+    @property
+    def permalink(self):
+        dt = self.pub_date
+        return '/{}/{:02d}/{}'.format(dt.year, dt.month, self.html_filename)
+
+
+def _slugify(text):
+    """Create a suitable slug for the given text"""
+    return re.sub(r'\W', '-', text).lower()
 
 
 def _read_config():
@@ -23,15 +99,15 @@ def _read_config():
 
     :return dict: the configuration dictionary
     """
-    log.info('Reading configuration...')
+    logger.log.info('Reading configuration...')
     cfg_file = os.path.join(PATHS['cwd'], 'config.yaml')
     if os.path.isfile(cfg_file):
         with open(cfg_file, 'r') as cf:
             config = yaml.safe_load(cf.read())
-            log.debug('Site-wide configuration: %s', config)
+            logger.log.debug('Site-wide configuration: %s', config)
             return config
     else:
-        log.warning('Warning: no configuration file found.')
+        logger.log.warning('Warning: no configuration file found.')
 
     return {}
 
@@ -60,7 +136,7 @@ def _is_valid_post_file(path):
 
 
 def _clean():
-    log.info('Cleaning generated files...')
+    logger.log.info('Cleaning generated files...')
     site_dir = PATHS['site']
     if os.path.isdir(site_dir):
         subprocess.call(['rm', '-r', site_dir])
@@ -70,64 +146,64 @@ def _build():
     config = _read_config()
 
     if not os.path.isdir(PATHS['site']):
-        log.debug('Creating site directory...')
+        logger.log.debug('Creating site directory...')
         subprocess.call(['mkdir', PATHS['site']])
 
-    log.info('Processing posts...')
+    logger.log.info('Processing posts...')
     ls = os.listdir(PATHS['posts'])
     post_path = lambda path: os.path.join(PATHS['posts'], path)
     posts = [Post(post_path(p)) for p in ls if _is_valid_post_file(p)]
 
     for post in posts:
-        log.debug('Rendering template for post %s', post.path)
+        logger.log.debug('Rendering template for post %s', post.path)
         template = JINJA_ENV.get_template(post.layout or 'article.html')
         html = template.render(site=config, post=post)
 
-        log.debug('Writing post to disk: %s', post)
+        logger.log.debug('Writing post to disk: %s', post)
         post_dir = os.path.dirname(post.permapath)
         if not os.path.isdir(post_dir):
-            log.debug('Creating post directory %s', post_dir)
+            logger.log.debug('Creating post directory %s', post_dir)
             subprocess.call(['mkdir', '-p', post_dir])
 
-        log.debug('Writing post to %s', post.permapath)
+        logger.log.debug('Writing post to %s', post.permapath)
         with open(post.permapath, 'w') as pout:
             pout.write(html)
 
-    log.info('Processing non-post files...')
+    logger.log.info('Processing non-post files...')
     for dirpath, subdirs, files in os.walk(PATHS['cwd']):
-        log.debug('Walking {}...'.format(dirpath))
+        logger.log.debug('Walking {}...'.format(dirpath))
         subdirs[:] = [d for d in subdirs if not d[0] in ['_', '.']]
         files[:] = [f for f in files if f.endswith('.html') or f.endswith('.xml')]
         rel_path = dirpath.replace(PATHS['cwd'], '').strip('/')
 
         for f in files:
-            log.debug('Processing %s', f)
+            logger.log.debug('Processing %s', f)
             template = JINJA_ENV.get_template(f)
             html = template.render(site=config, posts=posts)
 
             file_dir = os.path.join(PATHS['site'], rel_path)
 
             if not os.path.isdir(file_dir):
-                log.debug('Creating directory %s', file_dir)
+                logger.log.debug('Creating directory %s', file_dir)
                 subprocess.call(['mkdir', '-p', os.path.dirname(file_dir)])
 
             file_path = os.path.join(file_dir, f)
-            log.debug('Writing %s to %s', f, file_path)
+            logger.log.debug('Writing %s to %s', f, file_path)
             with open(file_path, 'w') as pout:
                 pout.write(html)
 
         if rel_path in config.get('keep', []):
-            log.debug('Keeping directory %s', rel_path)
+            logger.log.debug('Keeping directory %s', rel_path)
             subdirs[:] = []
 
             file_dir = os.path.join(PATHS['site'], rel_path)
 
             if not os.path.isdir(file_dir):
-                log.debug('Creating directory %s', file_dir)
+                logger.log.debug('Creating directory %s', file_dir)
                 subprocess.call(['mkdir', '-p', file_dir])
 
             copy_path = os.path.join(PATHS['site'], rel_path)
-            log.debug('Recursively copying %s to %s', dirpath, copy_path)
+            logger.log.debug('Recursively copying %s to %s', dirpath, copy_path)
             subprocess.call(['cp', '-r', dirpath, PATHS['site']])
 
 
@@ -135,7 +211,7 @@ def _build():
 @click.option('-v', '--verbose', count=True, help='Turn on verbose output.')
 @click.pass_context
 def cli(ctx, verbose):
-    init_logger(verbose)
+    logger.init_logger(verbose)
 
 
 @cli.command()
@@ -143,7 +219,7 @@ def cli(ctx, verbose):
 def init(ctx):
     """Initialize the current directory."""
     for d in [PATHS['posts'], PATHS['templates']]:
-        log.debug('Creating directory %s' % d)
+        logger.log.debug('Creating directory %s' % d)
         if not os.path.isdir(d):
             subprocess.call(['mkdir', d])
 
@@ -201,7 +277,7 @@ def preview(ctx, host, port):
 def new(ctx, title, layout):
     """Create a new post with the given title"""
     date = datetime.datetime.now().strftime('%Y-%m-%d')
-    slug = slugify(title)
+    slug = _slugify(title)
     filename = '{}-{}.md'.format(date, slug)
     full_path = os.path.join(PATHS['posts'], filename)
 
@@ -220,8 +296,8 @@ Your post content goes here.
             """.format(title=title, slug=slug, layout=layout)
             f.write(text)
 
-        log.info('Created {file}'.format(file=full_path))
+        logger.log.info('Created {file}'.format(file=full_path))
         click.secho("Created {file}".format(file=full_path), fg='green')
     else:
-        log.error('A post with that date and title already exists')
+        logger.log.error('A post with that date and title already exists')
         raise click.ClickException('A post with that date and title already exists')
