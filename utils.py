50b4d9721d558ec43c6f78be1b6ad6bd9996577a
Bill Israel <bill.israel@gmail.com>
2018-02-26 06:33:38 -0600

    Improve readability minorly

--- utils.py
+++ utils.py
@@ -3,28 +3,29 @@ import re
 
 
 def slugify(text):
-    """Create a suitable slug for the given text"""
+    """Create a good-enough slug for the given text"""
     return re.sub(r'\W', '-', text).lower()
 
 
-def is_valid_post_file(path):
+def is_valid_post_file(basename):
     """
-    Determines if the given path is valid for a post file.
+    Determines if the given filename is valid for a post file.
 
     The criteria:
         1. The file can't start with an underscore; these are ignored
         2. The file's name must match the pattern yyyy-mm-dd-*
         3. The file's extension must be a valid Markdown extension
 
-    :param path: The file path to validate
+    :param basename: The file name to validate
+    :type basename: string
     """
     post_pattern = r'^\d{4}-\d{2}-\d{2}-.*'
     markdown_extensions = ['md', 'markdown', 'mdown']
 
-    filename, ext = os.path.basename(path).rsplit('.', 1)
+    filename, ext = os.path.basename(basename).rsplit('.', 1)
 
-    ignored = not filename.startswith('_')
+    ignorable = filename.startswith('_')
     valid_filename = re.match(post_pattern, filename)
-    valid_ext = ext in markdown_extensions
+    valid_extension = ext in markdown_extensions
 
-    return ignored and valid_filename and valid_ext
+    return not ignorable and valid_filename and valid_extension
