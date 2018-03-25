9a449a15057b03a6baac6b5c5f931884635df6c0
Bill Israel <bill.israel@gmail.com>
2018-03-25 14:45:21 -0500

    Add test for new preview behavior

--- tests/test_models.py
+++ tests/test_models.py
@@ -259,3 +259,51 @@ def test_blog_build_and_clean(tmpdir):
     blog.clean()
     assert not os.path.isdir(str(site_path))
 
+
+def test_blog_build_and_clean_with_drafts(tmpdir):
+    path = tmpdir.mkdir('blog')
+    preview_path = path.mkdir('_preview')
+
+    # Set up a nanogen blog for posts
+    blog = models.Blog(str(path))
+    blog.init()
+
+    with mock.patch('subprocess.call'):
+        blog.new_post('Test post', draft=False)
+        blog.new_post('Draft post', draft=True)
+
+    post_template = path.join('_layout').join('post.html')
+    post_template.write("""\
+    <!doctype html>
+    <html>
+    <body>Post template would go here.</body>
+    </html>
+    """)
+
+    index_template = path.join('_layout').join('index.html')
+    index_template.write("""\
+    <!doctype html>
+    <html>
+    <body>Index template would go here.</body>
+    </html>
+    """)
+
+    blog_config = path.join('_layout').join('blog.cfg')
+    blog_config.write(example_config)
+
+    # Refresh the blog instance to better emulate real-world usage
+    blog = models.Blog(str(path), is_preview=True)
+    blog.build()
+
+    site_dir = [os.path.basename(str(file)) for file in preview_path.listdir()]
+    assert 'index.html' in site_dir
+
+    today = datetime.date.today()
+    expected_post_dir = preview_path.join('{}'.format(today.year)).join('{:02d}'.format(today.month))
+    generated_posts = [os.path.basename(str(file)) for file in expected_post_dir.listdir()]
+    assert len(generated_posts) == 2
+    assert 'test-post.html' in generated_posts
+    assert 'draft-post.html' in generated_posts
+
+    blog.clean()
+    assert not os.path.isdir(str(preview_path))
